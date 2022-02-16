28df892024132cd4fda496559f34b317b3aedb8d
lopezboje <98184776+lopezboje@users.noreply.github.com>
2022-02-16 03:10:20 -0500

    First Upload
    Uploading the app from the front-end so far. Need to upload the rest of the code that works on the back-end functionality.

---
+++ GPA Calculator/GPA Calculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GPA Calculator
+//
+//  Created by Joshua Lopez-Boutier on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var courseTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
